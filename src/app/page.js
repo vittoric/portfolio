268f28c273da3a997d925f19b8a0904ec0142c7f
vittoric <toricod22@gmail.com>
2024-02-15 16:48:00 +0100

    cambios...

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+'use client';
+import About from '../components/about/About';
+import Image from "next/image";
+import Spline from '@splinetool/react-spline';
+
+
+
+export default function Home() {
+  return (
+    <About/>
+  );
+}
---
+++ src/components/about/About.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div className="bg-cv-dark p-8 m-8 rounded-xl flex-col  w-1/2 flex md:flex-row gap-4 item-center justify-center mx-auto animate-fade-up animate-once animate-duration-1000 animate-delay-500">
+     <div>
+      <h1>About</h1>
+      <br/>
+      <p className="font-thin">
+        {" "}
+        As a student at 42 Madrid, I have a strong passion for coding and
+        developing new projects, and this repository is a showcase of my latest
+        work. Here you'll find a diverse range of projects, from simple scripts
+        to complex applications, covering various programming languages and
+        tools. Some of the projects include detailed documentation and
+        instructions for getting started, making them accessible to developers.
+        Whether you're a seasoned developer or just starting out, I invite you
+        to explore my code, fork the projects, and contribute as you see fit.
+        With your help, we can create something truly amazing together. Thank
+        you for stopping by, and happy coding!{" "}
+      </p>
+      </div>
+      
+    <Image src="/frog.png" className="bg-red-200 object-cover rounded-2xl" alt="42" width={200} height={200} />
+    </div>
+  );
+}
