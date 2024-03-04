573a021461a5350b55c7762bb8efa51f6c320b19
vittoric <toricod22@gmail.com>
2024-03-04 19:13:53 +0100

    div background

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 90deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
