98613d90127a673bf9e05cf940312fe3a152b6ba
vittoric <toricod22@gmail.com>
2024-03-13 20:11:42 +0100

    preloading

--- src/components/about/Landing.js
+++ src/components/about/Landing.js
@@ -1,12 +1,41 @@
 import Spline from "@splinetool/react-spline";
 import Link from "next/link";
+import { Fragment, useEffect, useState } from "react";
+import Image from "next/image";
+
 
 export default function Landing() {
   return (
    <Link href="/about">
-      <div className="h-screen">
-        <Spline scene="https://prod.spline.design/LMzEJWw7BUv9tfqj/scene.splinecode" />
+      <div className="h-screen ">
+        <SplineComponent />
       </div>
     </Link>
+  );
+}
+
+function SplineComponent() {
+  const [isSplineLoaded, setIsSplineLoaded] = useState(false);
+
+  
+
+  return (
+    <Fragment>
+      {!isSplineLoaded && (
+        <div className=" static flex items-center justify-center h-screen">
+        <Image
+          className="animate-pulse animate-infinite"
+          src="/vica.png"
+          alt="vica"
+          width={200}
+          height={200}
+        />
+        </div>
+      )}
+      <Spline 
+      className={isSplineLoaded || "hidden"}
+      onLoad={() => setTimeout (() => setIsSplineLoaded(true), 3000)}
+      scene="https://prod.spline.design/LMzEJWw7BUv9tfqj/scene.splinecode" />
+    </Fragment>
   );
 }
